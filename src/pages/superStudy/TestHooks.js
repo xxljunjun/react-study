fa7266beba3cfd01efc0c5d837812b792111b9c9
xxljunjun <liaojunjun@autel.com>
2021-06-18 15:21:12 +0800

    0

--- src/pages/superStudy/TestHooks.js
+++ src/pages/superStudy/TestHooks.js
@@ -1,5 +1,5 @@
 
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 //ES5写法
 // function TestHooks(props){
@@ -10,25 +10,23 @@ import React,{useState,useEffect} from 'react'
 //     )
 // }
 
-
-
 //ES6写法
-const TestHooks = props =>{
+const TestHooks = props => {
     //变量
-    let [count,setCount] = useState(1000)
-    let [list,setList] = useState([{name:"小溪流",id:1}])
-    let [msg,setMsg]= useState('hello word')
-    let timer =null
+    let [count, setCount] = useState(1000)
+    let [list, setList] = useState([{ name: "小溪流", id: 1 }])
+    let [msg, setMsg] = useState('hello word')
+    let timer = null
 
     //方法
-    let add = ()=>(
+    let add = () => (
         setCount(++count)
     )
-    let sub = ()=>(
+    let sub = () => (
         setCount(--count)
     )
-    let listRender =()=>(
-        list.map(ele=>(
+    let listRender = () => (
+        list.map(ele => (
             <div key={ele.id}>
                 <span>{ele.name}</span>
                 <span>-----</span>
@@ -36,31 +34,31 @@ const TestHooks = props =>{
             </div>
         ))
     )
-    
+
     //使用react的生命周期特性
     //语法：useEffect(fn,[])
     // 第二个参数是为数组，它相当于是 componentDidUpdate()
-    useEffect(()=>{
-        timer=setTimeout(()=>{
+    useEffect(() => {
+        timer = setTimeout(() => {
             // 相当于是 componentDidMount()
-            let res =[
-                {name:"小米",id:1},
-                {name:"小兰",id:2},
-                {name:"百变",id:3},
-                {name:"难受",id:4}
+            let res = [
+                { name: "小米", id: 1 },
+                { name: "小兰", id: 2 },
+                { name: "百变", id: 3 },
+                { name: "难受", id: 4 }
             ]
             setList(res)
-        },2000)
-        return ()=>{
+        }, 2000)
+        return () => {
             // 相当于是 componentWillUnmount()
             clearTimeout(timer)
         }
-    },[])
+    }, [])
 
-    useEffect(()=>{
-        setMsg(msg+"0")
+    useEffect(() => {
+        setMsg(msg + "0")
         return undefined
-    },[count])
+    }, [count])
     // 每当 count 发生变化时，这个副作用都会运行一遍
 
 
@@ -69,13 +67,14 @@ const TestHooks = props =>{
     return (
         <div>
             <h4>测试Hooks</h4>
+            <h4>hooks是用来解决函数式组件不存在生命周期的解决办法</h4>
             <h4>{count}</h4>
             <h4>{msg}</h4>
-            <button onClick={()=>add()}>点击按钮增加</button>
-            <button onClick={()=>sub()}>点击按钮减少</button>
-            <hr/>
+            <button onClick={() => add()}>点击按钮增加</button>
+            <button onClick={() => sub()}>点击按钮减少</button>
+            <hr />
             {
-               listRender() 
+                listRender()
             }
         </div>
     )
--- src/pages/superStudy/TestTypes.js
+++ src/pages/superStudy/TestTypes.js
@@ -1,61 +1,60 @@
 import React from "react"
 import PropTypes from "prop-types"
+export default class TestTypes extends React.Component {
+    render() {
+        return (
+            <>
+                <h4>测试propTypes和验证Props数据类型</h4>
+                <hr />
+                <Child1
+                    list={[1, 2, 3, 4, 5]}
+                    msg="hello child1"
+                    onchang2={() => console.log("test on change")}
+                    gender="男"
+                />
+                <hr />
+                <Child2 age={1000} />
+            </>
+        )
 
-class Child1 extends React.Component{
-    onChange(){
+    }
+}
+
+class Child1 extends React.Component {
+    onChange() {
         console.log('11')
     }
-    render(){
-        let { list ,msg ,onChange} = this.props
+    render() {
+        let { list, msg, onChange } = this.props
         return (
             <div>
                 <h4>我是child1子组件</h4>
                 {
-                    list.map(ele=>(
-                    <div key={ele}>{ele}</div>
+                    list.map(ele => (
+                        <div key={ele}>{ele}</div>
                     ))
                 }
-                <h4 onClick={()=>onChange()}>{msg}</h4>
+                <h4 onClick={() => onChange()}>{msg}</h4>
             </div>
-            
+
         )
     }
 }
-//给类组件Child1加props类型校验
-Child1.propTypes={
-    list:PropTypes.array.isRequired,
-    msg:PropTypes.string.isRequired,
-    onChange:PropTypes.func,
-    gender:PropTypes.oneOf(["男","女"]).isRequired
-}
-
-const Child2 =props=>(
+const Child2 = props => (
     <div>
         <h2>我是child2子组件</h2>
         <h2>{props.age}</h2>
     </div>
 )
-//给类组件Child1加props类型校验
-Child2.propTypes={
-    age: PropTypes.number.isRequired
-}
 
-export default class TestTypes extends React.Component{
-    render(){
-        return (
-            <>
-                <h4>测试propTypes和验证Props数据类型</h4>
-                <hr/>
-                <Child1
-                    list={[1,2,3,4,5]}
-                    msg="hello child1"
-                    onchang2={()=>console.log("test on change")}
-                    gender="男"
-                />
-                <hr/>
-                <Child2 age={1000}/>
-            </>
-        )
-        
-    }
+//给类组件Child1加props类型校验
+Child1.propTypes = {
+    list: PropTypes.array.isRequired,
+    msg: PropTypes.string.isRequired,
+    onChange: PropTypes.func,
+    gender: PropTypes.oneOf(["男", "女"]).isRequired
+}
+//给类组件Child1加props类型校验
+Child2.propTypes = {
+    age: PropTypes.number.isRequired
 }
